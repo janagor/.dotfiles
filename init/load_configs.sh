4c50275ce02111217a91788f31f6665ed811cc55
jgorski <01178321@pw.edu.pl>
2024-11-09 16:39:20 +0100

    fixed symlinks

--- init/load_configs.sh
+++ init/load_configs.sh
@@ -1,12 +1,10 @@
-
 ln -s ~/.dotfiles/nvim ~/.config/
 
-
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/tmux/.tmux.conf ~/
 
 rm ~/.zshrc
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/zsh/.zshrc ~/
 
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -14,6 +12,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 /bin/zsh ~/.zshrc
 
-
-
-ln -s ~/.dotfiles/alacritty ~/.config/alacritty
+ln -s ~/.dotfiles/alacritty ~/.config/
